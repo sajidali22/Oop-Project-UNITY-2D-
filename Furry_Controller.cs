12cc1edad71e4a51b0f749b6478f2895f01188cf
notaCoder69 <52592276+notaCoder69@users.noreply.github.com>
2019-07-06 11:43:35 +0500

    Player Movement Script
    Isko attach karo main player se

---
+++ Furry_Controller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furry_Controller : MonoBehaviour
+{
+    bool isInvincible;
+    float invincibleTimer;
+    public float incvincibleTime = 2.0f;
+    Vector2 position;
+    Rigidbody2D rigidbody2d;
+    public int maxHealth = 5;
+    public int Health { get { return currentHealth; } }
+    int currentHealth;
+    public float speed;
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = incvincibleTime;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+    
+}
